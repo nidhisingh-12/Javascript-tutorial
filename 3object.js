518857f77452a8a265b08d56b94b761b36aef2f2
Nidhi Singh <93303985+nidhisingh-12@users.noreply.github.com>
2023-04-21 19:14:07 +0530

    Add files via upload

---
+++ 3object.js
@@ -0,0 +1,10 @@
+//non-primitvie datatype - objects in js
+
+const item = {
+    "nidhi": true,
+    "neha": false,
+    "kumar": 63,
+    "sohan":undefined
+}
+console.log(item["hello"]) //undefined becasue hello not present in item
+console.log(item["kumar"])
