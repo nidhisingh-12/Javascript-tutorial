45d4a8626dd160b4bdcd9ac156566e5c4dc1f9b5
Nidhi Singh <93303985+nidhisingh-12@users.noreply.github.com>
2023-04-21 19:13:29 +0530

    Add files via upload

---
+++ 1varletcons.js
@@ -0,0 +1,22 @@
+// var a = 45;
+// var b = "harry";
+// var c = null
+// var d = undefined
+// {
+//     let b = 'this'
+//     console.log(b)
+// }
+// console.log(b) 
+
+let a = 45;
+const author = "harry"
+// let author = 5   const can't changed
+let b = "harry";
+let c = null
+// let b = 4 we can't redeclared the same variable
+let d = undefined
+{
+        let b = 'this'
+        console.log(b)
+    }
+console.log(b) 
