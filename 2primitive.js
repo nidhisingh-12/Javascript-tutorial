259026816b07c67a89dbc5f0757b6962bc5d2165
Nidhi Singh <93303985+nidhisingh-12@users.noreply.github.com>
2023-04-21 19:13:47 +0530

    Add files via upload

---
+++ 2primitive.js
@@ -0,0 +1,10 @@
+// primitive js
+let a = null;
+let b = 345;
+let c = true;
+let d = BigInt("345");
+let e = "Nidhi";
+let f = Symbol("I am nice symbol")
+let g = undefined
+console.log(a,b,c,d,e,f,g)
+console.log(typeof d) //check type of primitive
