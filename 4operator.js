f399eef6b1be572a57e26c06592c684b64d529a8
Nidhi Singh <93303985+nidhisingh-12@users.noreply.github.com>
2023-04-22 23:07:19 +0530

    Add files via upload

---
+++ 4operator.js
@@ -0,0 +1,29 @@
+console.log("Arithmetic Operator")
+let a = 16;
+let b = 4;
+console.log("a + b = ", a+b)
+console.log("a - b = ", a-b)
+console.log("a * b = ", a*b)
+console.log("a % b = ", a%b)
+console.log("a / b = ", a/b)
+console.log("a ** b = ", a**b)
+console.log("a++ = ", a++)
+console.log("a -- = ", a--)
+
+
+console.log("Comparison operator")
+let c1 = 6;
+let  c2 = 6;
+console.log("c1 == c2", c1 == c2)
+console.log("c1 != c2", c1 != c2)
+console.log("c1 === c2", c1 === c2)
+console.log("c1 !== c2", c1 !== c2)
+console.log("c1 >= c2", c1 >= c2)
+
+console.log("Logical Operator")
+let x = 5;
+let  y = 6;
+console.log(x<y && x == 5)
+console.log(x<y || x == 5)
+console.log(!false)
+console.log(!true)
