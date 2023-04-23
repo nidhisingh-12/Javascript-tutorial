11230d2c686a58e66207e3a84fd2e273533fb659
Nidhi Singh <93303985+nidhisingh-12@users.noreply.github.com>
2023-04-23 20:36:47 +0530

    Create 5conditional.js

---
+++ 5conditional.js
@@ -0,0 +1,16 @@
+// let a = prompt("Hey what your age?") //taking age from user
+// a = Number.parseInt(a)  //converting the string into a number
+// if(a<0){
+//     alert("This is a invalid age");
+// }
+// else if (a<18 && a>=9){
+//   alert("after 18 you can drive");
+// }
+// else
+// {
+//   alert("you can drive")
+// }
+
+
+let a = 15;
+console.log("you can", (a<18 ? "not drive" : "drive"))
