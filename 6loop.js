a0c384643e1574b72cab06027d31b12a7df9c373
Nidhi Singh <93303985+nidhisingh-12@users.noreply.github.com>
2023-04-24 23:51:24 +0530

    Create 6loop.js

---
+++ 6loop.js
@@ -0,0 +1,15 @@
+//initialization of loop in js
+for(let i =0; i<5; i++)
+  {
+    console.log(i)
+  }
+
+//To add first n natural number 
+let sum =0;
+let n = prompt("enter the value of n")
+n=Number.parseInt(n)
+for(let i=1; i<=n; i++)
+  {
+    sum+=i
+  }
+console.log(sum)
