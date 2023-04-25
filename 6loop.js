959d2d264db5bd24161af6883b223836c6fb2385
Nidhi Singh <93303985+nidhisingh-12@users.noreply.github.com>
2023-04-25 23:45:13 +0530

    Update 6loop.js

--- 6loop.js
+++ 6loop.js
@@ -1,15 +1,33 @@
-//initialization of loop in js
-for(let i =0; i<5; i++)
-  {
-    console.log(i)
-  }
+initialization of for loop
+// for(let i =0; i<5; i++)
+//   {
+//     console.log(i)
+//   }
 
-//To add first n natural number 
-let sum =0;
-let n = prompt("enter the value of n")
-n=Number.parseInt(n)
-for(let i=1; i<=n; i++)
+// //To add first n natural number 
+// let sum =0;
+// let n = prompt("enter the value of n")
+// n=Number.parseInt(n)
+// for(let i=1; i<=n; i++)
+//   {
+//     sum+=i
+//   }
+// console.log(sum)  
+
+
+// //for in loop
+// let obj = {
+//   nidhi: 90,
+//   sneha: 87,
+//   ritika: 80,
+// }
+// for(let a in obj) {
+//   console.log("Marks of "+a+"are "+obj[a])
+// } 
+
+
+//for of loop -> it should be iteratorable
+for(let b of "nidhi")
   {
-    sum+=i
+    console.log(b)
   }
-console.log(sum)
