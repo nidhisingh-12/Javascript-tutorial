941f85c673b1c4163a7fefca3ecd09851dc31afb
Nidhi Singh <93303985+nidhisingh-12@users.noreply.github.com>
2023-04-26 23:46:09 +0530

    Update 6loop.js

--- 6loop.js
+++ 6loop.js
@@ -31,3 +31,24 @@ for(let b of "nidhi")
   {
     console.log(b)
   }
+
+
+// while loop declaration
+// let n = prompt("enter the value of n")
+// n = Number.parseInt(n)
+// let i=0; 
+// while(i<n)
+//   {
+//     console.log(i)
+//     i++;
+//   }  
+
+
+// do-while loop
+let n = prompt("enter the value of n")
+n = Number.parseInt(n)
+let i=0; 
+do{
+  console.log(i)
+  i++;
+}while(i<n)
