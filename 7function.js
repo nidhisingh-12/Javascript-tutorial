491b33cdf2915d3e97f364e10de9660a23b7ea91
Nidhi Singh <93303985+nidhisingh-12@users.noreply.github.com>
2023-04-27 23:51:00 +0530

    Create 7function.js

---
+++ 7function.js
@@ -0,0 +1,21 @@
+// function declaration in javascript
+// function oneplusavg(x,y){
+//   console.log("Done")
+//   return (x+y)/2
+// }
+// let a = 1;
+// let b = 3;
+// console.log(" average of a and b is",oneplusavg(a,b)) //invoke the function 
+
+
+//other way of using function
+const sum = (p,q)=>{ //arrow function 
+  return p+q
+} 
+console.log(sum(9,7))
+
+//other function example 
+const hello = ()=>{
+  console.log("hey how are you.")
+} 
+hello();
