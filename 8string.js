a0cead3617953337719720fbc1b0be4de5c71682
Nidhi Singh <93303985+nidhisingh-12@users.noreply.github.com>
2023-04-28 23:11:01 +0530

    Create 8string.js

---
+++ 8string.js
@@ -0,0 +1,21 @@
+//we can used double quotes and single quotes also 
+let name = "nidhi"
+console.log(name.length)
+
+let friend = 'neha'
+console.log(friend)
+
+let family = "singh"
+console.log(family[0]) 
+console.log(family[1]) 
+
+//template literals
+let boy1 = "pramod"
+let boy2 = "nitin"
+let sentence = `${boy1} is a friend of ${boy2}`
+console.log(sentence)
+
+//escape sequence characters
+let fruit = "Bana\"na" //single quotes also can be used
+console.log(fruit)
+console.log(fruit.length)
