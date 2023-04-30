53c33e1af29cdba1338caecd7b35425049760758
Nidhi Singh <93303985+nidhisingh-12@users.noreply.github.com>
2023-04-30 22:58:09 +0530

    Create 9Array.js

---
+++ 9Array.js
@@ -0,0 +1,11 @@
+let marks_class_12 = [91,23,45,64,24,false,"Not present"]
+console.log(marks_class_12)
+console.log(marks_class_12[0])
+console.log(marks_class_12[1])
+console.log(marks_class_12[2])
+console.log(marks_class_12[3])
+console.log(marks_class_12[4])
+console.log("the legnth og marks_class_12 is", marks_class_12.length)
+marks_class_12[4]= 89
+marks_class_12[3] = 96
+console.log(marks_class_12)
