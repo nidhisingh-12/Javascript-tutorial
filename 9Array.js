83efec7e15aaa6b62276af29869a00b6817e8042
Nidhi Singh <93303985+nidhisingh-12@users.noreply.github.com>
2023-05-01 23:55:33 +0530

    Update 9Array.js

--- 9Array.js
+++ 9Array.js
@@ -5,6 +5,7 @@ console.log(marks_class_12[1])
 console.log(marks_class_12[2])
 console.log(marks_class_12[3])
 console.log(marks_class_12[4])
+console.log(marks_class_12[6])
 console.log("the legnth og marks_class_12 is", marks_class_12.length)
 marks_class_12[4]= 89
 marks_class_12[3] = 96
